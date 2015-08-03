d65e07efaf06b2088b40d43feaf886a7fd95e110
darknightmare <rubentkd98@gmail.com>
2015-08-04 01:08:59 +0200

    piccolo: Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,79 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+-include device/bq/msm8939-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/bq/piccolo
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := piccolo,aquarism5
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/bq/piccolo
+TARGET_KERNEL_CONFIG := cyanogenmod_piccolo_defconfig
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
+BOARD_RAMDISK_OFFSET     := 0x81000000
+BOARD_KERNEL_TAGS_OFFSET := 0x80000100
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# CPU
+TARGET_CPU_CORTEX_A53 := true
+
+# GPS
+TARGET_GPS_HAL_PATH := $(DEVICE_PATH)/gps
+TARGET_NO_RPC := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 33554432    # 32MB
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432    # 32MB
+BOARD_PERSISTIMAGE_PARTITION_SIZE  := 33554432    # 32MB
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1610612736  # 1536MB
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 13363018752 # 12744MB (BOARD_USERDATAIMAGE_PARTITION_SIZE - 16384 for crypto footer)
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 419430400   # 400MB
+BOARD_FLASH_BLOCK_SIZE             := 131072      # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_DENSITY := xhdpi
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/bq/piccolo/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    system.te \
+    system_server.te \
+    wcnss_service.te
+
+# Wifi
+TARGET_USES_WCNSS_CTRL := true
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+
+# inherit from the proprietary version
+-include vendor/bq/piccolo/BoardConfigVendor.mk
---
+++ liblight/lights.c
@@ -0,0 +1,353 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2014 The  Linux Foundation. All rights reserved.
+ * Copyright (C) 2014-2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+//#define LOG_NDEBUG 0
+#define LOG_TAG "lights"
+
+#include <cutils/log.h>
+
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#include <sys/types.h>
+
+#include <hardware/lights.h>
+
+/******************************************************************************/
+
+static pthread_once_t g_init = PTHREAD_ONCE_INIT;
+static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
+static struct light_state_t g_attention;
+static struct light_state_t g_notification;
+static struct light_state_t g_battery;
+
+char const *const LCD_FILE
+        = "/sys/class/leds/lcd-backlight/brightness";
+
+const char *const BUTTONS_FILE
+        = "/sys/class/leds/button-backlight/brightness";
+
+char const *const RED_LED_FILE
+        = "/sys/class/leds/red/brightness";
+
+char const *const RED_BLINK_FILE
+        = "/sys/class/leds/red/blink";
+
+char const *const GREEN_LED_FILE
+        = "/sys/class/leds/green/brightness";
+
+char const *const GREEN_BLINK_FILE
+        = "/sys/class/leds/green/blink";
+
+char const *const BLUE_LED_FILE
+        = "/sys/class/leds/blue/brightness";
+
+char const *const BLUE_BLINK_FILE
+        = "/sys/class/leds/blue/blink";
+
+/**
+ * device methods
+ */
+
+void init_globals(void)
+{
+    // init the mutex
+    pthread_mutex_init(&g_lock, NULL);
+}
+
+static int
+write_str(char const *path, char *value)
+{
+    int fd;
+    static int already_warned = 0;
+
+    fd = open(path, O_RDWR);
+    if (fd >= 0) {
+        ssize_t amt = write(fd, value, (size_t)strlen(value));
+        close(fd);
+        return amt == -1 ? -errno : 0;
+    } else {
+        if (already_warned == 0) {
+            ALOGE("write_str failed to open %s\n", path);
+            already_warned = 1;
+        }
+        return -errno;
+    }
+}
+
+static int
+write_int(char const *path, int value)
+{
+    char buffer[20];
+    snprintf(buffer, sizeof(buffer), "%d\n", value);
+    return write_str(path, buffer);
+}
+
+static int
+is_lit(struct light_state_t const *state)
+{
+    return state->color & 0x00ffffff;
+}
+
+static int
+rgb_to_brightness(const struct light_state_t *state)
+{
+    int color = state->color & 0x00ffffff;
+    return ((77 * ((color >> 16) & 0xff))
+            + (150 * ((color >> 8) & 0xff))
+            + (29 * (color & 0xff))) >> 8;
+}
+
+static int
+set_speaker_light_locked(struct light_device_t *dev,
+        struct light_state_t const *state)
+{
+    int red, green, blue;
+    int blink;
+    int onMS, offMS;
+    unsigned int colorRGB;
+
+    if (!dev) {
+        return -1;
+    }
+
+    write_int(RED_LED_FILE, 0);
+    write_int(GREEN_LED_FILE, 0);
+    write_int(BLUE_LED_FILE, 0);
+
+    if (state == NULL) {
+        return 0;
+    }
+
+    switch (state->flashMode) {
+        case LIGHT_FLASH_TIMED:
+            onMS = state->flashOnMS;
+            offMS = state->flashOffMS;
+            break;
+        case LIGHT_FLASH_NONE:
+        default:
+            onMS = 0;
+            offMS = 0;
+            break;
+    }
+
+    colorRGB = state->color;
+
+    ALOGV("set_speaker_light_locked mode %d, colorRGB=%08X, onMS=%d, offMS=%d\n",
+            state->flashMode, colorRGB, onMS, offMS);
+
+    red = (colorRGB >> 16) & 0xFF;
+    green = (colorRGB >> 8) & 0xFF;
+    blue = colorRGB & 0xFF;
+
+    if (onMS > 0 && offMS > 0 && !(
+          (red == green && green == blue) ||
+          (red == green && blue == 0) ||
+          (red == blue && green == 0) ||
+          (green == blue && red == 0) ||
+          (blue == 0 && red == 0) ||
+          (green == 0 && red == 0) ||
+          (green == 0 && blue == 0)
+        )) {
+        // Blinking only works if all active component colors have
+        // the same brightness value
+        offMS = 0;
+    }
+
+    if (onMS > 0 && offMS > 0) {
+        blink = 1;
+    } else {
+        blink = 0;
+    }
+
+    // Power rails are in a workqueue, give the kernel time to bring them
+    // up before starting the blinks, or those'll be lost
+    usleep(500*1000);
+    if (red) write_int(RED_BLINK_FILE, blink);
+    if (green) write_int(GREEN_BLINK_FILE, blink);
+    if (blue) write_int(BLUE_BLINK_FILE, blink);
+
+    return 0;
+}
+
+static void
+handle_speaker_light_locked(struct light_device_t *dev)
+{
+    set_speaker_light_locked(dev, NULL);
+    if (is_lit(&g_attention)) {
+        set_speaker_light_locked(dev, &g_attention);
+    } else if (is_lit(&g_notification)) {
+        set_speaker_light_locked(dev, &g_notification);
+    } else {
+        set_speaker_light_locked(dev, &g_battery);
+    }
+}
+
+static int
+set_light_backlight(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    int err = 0;
+    int brightness = rgb_to_brightness(state);
+
+    pthread_mutex_lock(&g_lock);
+
+    err = write_int(LCD_FILE, brightness);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return err;
+}
+
+static int
+set_light_buttons(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    int err = 0;
+    int brightness = rgb_to_brightness(state);
+
+    pthread_mutex_lock(&g_lock);
+
+    err = write_int(BUTTONS_FILE, brightness);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return err;
+}
+
+static int
+set_light_attention(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    pthread_mutex_lock(&g_lock);
+
+    g_attention = *state;
+    if (state->flashMode == LIGHT_FLASH_HARDWARE) {
+        if (g_attention.flashOnMS > 0 && g_attention.flashOffMS == 0) {
+            g_attention.flashMode = LIGHT_FLASH_NONE;
+        }
+    } else if (state->flashMode == LIGHT_FLASH_NONE) {
+        g_attention.color = 0;
+    }
+    handle_speaker_light_locked(dev);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return 0;
+}
+
+static int
+set_light_notifications(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    pthread_mutex_lock(&g_lock);
+
+    g_notification = *state;
+    handle_speaker_light_locked(dev);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return 0;
+}
+
+static int
+set_light_battery(struct light_device_t *dev,
+        const struct light_state_t *state)
+{
+    pthread_mutex_lock(&g_lock);
+
+    g_battery = *state;
+    handle_speaker_light_locked(dev);
+
+    pthread_mutex_unlock(&g_lock);
+
+    return 0;
+}
+
+/** Close the lights device */
+static int
+close_lights(struct light_device_t *dev)
+{
+    if (dev) {
+        free(dev);
+    }
+    return 0;
+}
+
+/******************************************************************************/
+
+/**
+ * module methods
+ */
+
+/** Open a new instance of a lights device using name */
+static int open_lights(const struct hw_module_t *module, const char *name,
+        struct hw_device_t **device)
+{
+    int (*set_light)(struct light_device_t *dev,
+            const struct light_state_t *state);
+
+    if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
+        set_light = set_light_backlight;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
+    else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
+        set_light = set_light_notifications;
+    else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
+        set_light = set_light_attention;
+    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
+        set_light = set_light_battery;
+    else
+        return -EINVAL;
+
+    pthread_once(&g_init, init_globals);
+
+    struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+    memset(dev, 0, sizeof(*dev));
+
+    dev->common.tag = HARDWARE_DEVICE_TAG;
+    dev->common.version = 0;
+    dev->common.module = (struct hw_module_t*)module;
+    dev->common.close = (int (*)(struct hw_device_t*))close_lights;
+    dev->set_light = set_light;
+
+    *device = (struct hw_device_t*)dev;
+    return 0;
+}
+
+static struct hw_module_methods_t lights_module_methods = {
+    .open =  open_lights,
+};
+
+/*
+ * The lights Module
+ */
+struct hw_module_t HAL_MODULE_INFO_SYM = {
+    .tag = HARDWARE_MODULE_TAG,
+    .version_major = 1,
+    .version_minor = 0,
+    .id = LIGHTS_HARDWARE_MODULE_ID,
+    .name = "BQ Lights Module",
+    .author = "The CyanogenMod Project",
+    .methods = &lights_module_methods,
+};
